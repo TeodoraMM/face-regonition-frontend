0504d7654b6ab6abe7409e698110e4edcced70d7
TeodoraMM <teodora_mezei12@yahoo.com>
2023-12-28 18:30:23 +0200

    updated library

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+import brain from './brain.png'
+const Logo = () => {
+    return (
+        <div className="ma4 nt0">
+            <Tilt className="br2 shadow-2 tiltElem" style={{ height: '100px',width: '100px' }}>
+                <div className="pa3">
+                    <img style={{paddingTop:'5px'}} src={brain} alt="logo"/>
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../Logo/Logo';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if (isSignedIn) {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+      );
+    } else {
+      return (
+          <nav className="" style={{display: 'flex'}}>
+          <a style={{'marginRight': 'auto'}} href="#"> 
+            <Logo/>
+          </a>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+            <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>          
+        </nav>
+      );
+    }
+}
+
+export default Navigation;
